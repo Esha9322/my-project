9cbedfe4158eb2dc67c98c18361107e4fa771472
Esha9322 <eshavasale99@gmail.com>
2025-07-18 16:33:58 +0530

    Initial commit for AWS 3-tier application

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.chqykk4y4nd5.eu-west-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'eshavasale',
+    DB_DATABASE : 'webappdb'
+});
